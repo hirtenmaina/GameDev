b39e00696d31154a9ef67dad9af6ec94e59967f8
Tatiana <sttatrub@stud.h-da.de>
2015-05-04 13:32:48 +0200

    no message

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
 
+	public GameObject prefabProjectile;
+	public float velocityMult = 4.0;
 	private GameObject LaunchPoint;
+	private bool aimingMode;
+	private GameObject projectile;
+	private Vector3 launchPos;
 
 	void Awake (){
 		Transform LaunchPointTrans = transform.Find("LaunchPoint"); //only searches children
 		LaunchPoint = LaunchPointTrans.gameObject;
 		LaunchPoint.SetActive (false);
+		launchPos = LaunchPointTrans.position;
 	}
 
 	void OnMouseEnter()	{
@@ -19,4 +25,54 @@ public class Slingshot : MonoBehaviour {
 	LaunchPoint.SetActive (false);
 	}
 
+
+	void OnMouseDown(){
+	// set the game to aiming mode 
+		aimingMode = true;
+	//initiate a projectile at launchpoint
+		projectile = Instantiate (prefabProjectile) as GameObject;
+		projectile.transform.position = launchPos;
+	//Switch off gravity 
+		projectile.GetComponent<Rigidbody> ().isKinematic = true;
+	}
+
+	void Update(){
+		if (!aimingMode)return;
+
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = - Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+
+		Vector3 mouseDelta = mousePos3D - launchPos;
+
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+		mouseDelta = Vector3.ClampMagnitude (mouseDelta, maxMagnitude);
+
+		projectile.transform.position = launchPos + mouseDelta;
+	
+	if (Input.GetMouseButtonUp (0)) {
+			aimingMode = false;
+			projectile.GetComponent<Rigidbody> ().isKinematic = false;
+			projectile.GetComponent<Rigidbody> ().velocity = - mouseDelta*velocityMult;
+	}
+	
+	}
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
